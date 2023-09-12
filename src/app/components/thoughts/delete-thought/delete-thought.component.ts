798101d405abdb3678eb7495eee812a3e5479e63
carolrosatto <carolrosatto@gmail.com>
2023-09-11 21:37:40 -0300

    Criação do componente de deletar pensamento

---
+++ src/app/components/thoughts/delete-thought/delete-thought.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Thought } from '../thoughts';
+import { ThougthsService } from '../thougths.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-delete-thought',
+  templateUrl: './delete-thought.component.html',
+  styleUrls: ['./delete-thought.component.css']
+})
+export class DeleteThoughtComponent implements OnInit {
+
+  thought: Thought = {
+    id: 0,
+    content: '',
+    author: '',
+    model: ''
+  }
+
+  constructor(
+    private service: ThougthsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.searchById(parseInt(id!)).subscribe((thought) => {
+      this.thought = thought;
+    })
+  }
+
+  deleteThought() {
+    if(this.thought.id) {
+      this.service.delete(this.thought.id).subscribe(() => {
+        this.router.navigate(['/listThought']);
+      })
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/listThought']);
+  }
+
+}
